d3049792d0158643a1632b7e3d6797fac4e0b62b
04041b <65881086+04041b@users.noreply.github.com>
2025-02-18 14:21:28 -0800

    add code for button trigger

--- sketch_main/sketch_main.ino
+++ sketch_main/sketch_main.ino
@@ -1,9 +1,19 @@
+const int buttonPin = 2;  // the number of the pushbutton pin
+
+
 void setup() {
   // put your setup code here, to run once:
-
+    // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    Serial.print("button pressed")
+  }
+  else {
+    
+  }
 }
