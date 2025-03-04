d4f6b59eb51be72115d44e6c95550d6501075001
04041b <65881086+04041b@users.noreply.github.com>
2025-03-04 14:22:24 -0800

    uncomment, solve hardware issues

--- sketch_main/sketch_main.ino
+++ sketch_main/sketch_main.ino
@@ -12,7 +12,7 @@ const int DATA_TAG_SIZE = 8; // 8byte tag
 const int CHECKSUM_SIZE = 2; // 2byte checksum
 
 SoftwareSerial ssrfid = SoftwareSerial(4,5); 
-SoftwareSerial df1101sSerial(0,1); //RX, TX
+SoftwareSerial df1101sSerial(10,9); //RX, TX
 uint8_t buffer[BUFFER_SIZE]; // used to store an incoming data frame 
 int buffer_index = 0;
 
@@ -43,81 +43,81 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("looping");
-//   buttonState = digitalRead(buttonPin);
-//   unsigned id_num = readNow();
-//   if (buttonState == LOW) { //WRITE MODE: RECORD AUDIO
-//     Serial.print("button pressed \n");
-//     df1101s.start();
-//     delay(1000);
-//     String filename = df1101s.saveRec();
+  buttonState = digitalRead(buttonPin);
+  unsigned id_num = readNow();
+  if (buttonState == LOW) { //WRITE MODE: RECORD AUDIO
+    Serial.print("button pressed \n");
+    df1101s.start();
+    delay(1000);
+    String filename = df1101s.saveRec();
 
-//     //trigger write mode
-
-
-//   }
-//   else { //READ MODE: PLAY AUDIO
-//     //read mode always
-//     //Serial.print("button not pressed \n");
-//     int will_sound = LOW;
-//      if (id_num!=0 && (millis()-last_audio_time)> rfid_debounce_time_ms ) {  //if reading valid rfid
-//       tone(9,262,250);
-//       last_audio_time = millis();
-//      }
-
-//     if (id_num!=0) {
-//      Serial.print(id_num);
-//      Serial.print("\n");
-//     }
-
-//   }
-// }
+    //trigger write mode
 
 
-// boolean writeToEEProm(String content) {
-//   int i=0;
-//   while (i<32767) {
-//   if (EEPROM.read(i) !=0) {
-//     EEPROM.put(i,content);
-//   }
-//   }
-// }
+  }
+  else { //READ MODE: PLAY AUDIO
+    //read mode always
+    //Serial.print("button not pressed \n");
+    int will_sound = LOW;
+     if (id_num!=0 && (millis()-last_audio_time)> rfid_debounce_time_ms ) {  //if reading valid rfid
+      tone(9,262,250);
+      last_audio_time = millis();
+     }
+
+    if (id_num!=0) {
+     Serial.print(id_num);
+     Serial.print("\n");
+    }
+
+  }
+}
 
 
-// unsigned readNow() {
-//     if (ssrfid.available() > 0){
-//     bool call_extract_tag = false;
+boolean writeToEEProm(String content) {
+  int i=0;
+  while (i<32767) {
+  if (EEPROM.read(i) !=0) {
+    EEPROM.put(i,content);
+  }
+  }
+}
+
+
+unsigned readNow() {
+    if (ssrfid.available() > 0){
+    bool call_extract_tag = false;
     
-//     int ssvalue = ssrfid.read(); // read 
-//     if (ssvalue == -1) { // no data was read
-//       return;
-//     }
+    int ssvalue = ssrfid.read(); // read 
+    if (ssvalue == -1) { // no data was read
+      return;
+    }
 
-//     if (ssvalue == 2) { // RDM630/RDM6300 found a tag => tag incoming 
-//       buffer_index = 0;
-//     } else if (ssvalue == 3) { // tag has been fully transmitted       
-//       call_extract_tag = true; // extract tag at the end of the function call
-//     }
+    if (ssvalue == 2) { // RDM630/RDM6300 found a tag => tag incoming 
+      buffer_index = 0;
+    } else if (ssvalue == 3) { // tag has been fully transmitted       
+      call_extract_tag = true; // extract tag at the end of the function call
+    }
 
-//     if (buffer_index >= BUFFER_SIZE) { // checking for a buffer overflow (It's very unlikely that an buffer overflow comes up!)
-//       Serial.println("Error: Buffer overflow detected! ");
-//       return;
-//     }
+    if (buffer_index >= BUFFER_SIZE) { // checking for a buffer overflow (It's very unlikely that an buffer overflow comes up!)
+      Serial.println("Error: Buffer overflow detected! ");
+      return;
+    }
     
-//     buffer[buffer_index++] = ssvalue; // everything is alright => copy current value to buffer
+    buffer[buffer_index++] = ssvalue; // everything is alright => copy current value to buffer
 
-//     if (call_extract_tag == true) {
-//       if (buffer_index == BUFFER_SIZE) {
-//         unsigned tag = extract_tag();
-//         return tag;
-//       } else { // something is wrong... start again looking for preamble (value: 2)
-//         buffer_index = 0;
-//         return;
-//       }
-//     }    
-//   }
-//   else {
-//     return 0; // return 0 when none exist
-//   }    
+    if (call_extract_tag == true) {
+      if (buffer_index == BUFFER_SIZE) {
+        unsigned tag = extract_tag();
+        return tag;
+      } else { // something is wrong... start again looking for preamble (value: 2)
+        buffer_index = 0;
+        return;
+      }
+    }    
+  }
+  else {
+    return 0; // return 0 when none exist
+  }    
 }
 
 unsigned extract_tag() {
@@ -129,49 +129,49 @@ unsigned extract_tag() {
     uint8_t msg_tail = buffer[13];
 
     // print message that was sent from RDM630/RDM6300
-    // Serial.println("--------");
+    Serial.println("--------");
 
-    // Serial.print("Message-Head: ");
-    // Serial.println(msg_head);
+    Serial.print("Message-Head: ");
+    Serial.println(msg_head);
 
-    // Serial.println("Message-Data (HEX): ");
-    // for (int i = 0; i < DATA_VERSION_SIZE; ++i) {
-    //   Serial.print(char(msg_data_version[i]));
-    // }
-    // Serial.println(" (version)");
+    Serial.println("Message-Data (HEX): ");
+    for (int i = 0; i < DATA_VERSION_SIZE; ++i) {
+      Serial.print(char(msg_data_version[i]));
+    }
+    Serial.println(" (version)");
     
-    // for (int i = 0; i < DATA_TAG_SIZE; ++i) {
-    //   Serial.print(char(msg_data_tag[i]));
-    // }
-    // Serial.println(" (tag)");
+    for (int i = 0; i < DATA_TAG_SIZE; ++i) {
+      Serial.print(char(msg_data_tag[i]));
+    }
+    Serial.println(" (tag)");
 
-    // Serial.print("Message-Checksum (HEX): ");
-    // for (int i = 0; i < CHECKSUM_SIZE; ++i) {
-    //   Serial.print(char(msg_checksum[i]));
-    // }
-    // Serial.println("");
+    Serial.print("Message-Checksum (HEX): ");
+    for (int i = 0; i < CHECKSUM_SIZE; ++i) {
+      Serial.print(char(msg_checksum[i]));
+    }
+    Serial.println("");
 
-    // Serial.print("Message-Tail: ");
-    // Serial.println(msg_tail);
+    Serial.print("Message-Tail: ");
+    Serial.println(msg_tail);
 
-    // Serial.println("--");
+    Serial.println("--");
 
     long tag = hexstr_to_value(msg_data_tag, DATA_TAG_SIZE);
-    // Serial.print("Extracted Tag: ");
-    // Serial.println(tag);
+    Serial.print("Extracted Tag: ");
+    Serial.println(tag);
 
-    // long checksum = 0;
-    // for (int i = 0; i < DATA_SIZE; i+= CHECKSUM_SIZE) {
-    //   long val = hexstr_to_value(msg_data + i, CHECKSUM_SIZE);
-    //   checksum ^= val;
-    // }
-    // Serial.print("Extracted Checksum (HEX): ");
-    // Serial.print(checksum, HEX);
-    // if (checksum == hexstr_to_value(msg_checksum, CHECKSUM_SIZE)) { // compare calculated checksum to retrieved checksum
-    //   Serial.print(" (OK)"); // calculated checksum corresponds to transmitted checksum!
-    // } else {
-    //   Serial.print(" (NOT OK)"); // checksums do not match
-    // }
+    long checksum = 0;
+    for (int i = 0; i < DATA_SIZE; i+= CHECKSUM_SIZE) {
+      long val = hexstr_to_value(msg_data + i, CHECKSUM_SIZE);
+      checksum ^= val;
+    }
+    Serial.print("Extracted Checksum (HEX): ");
+    Serial.print(checksum, HEX);
+    if (checksum == hexstr_to_value(msg_checksum, CHECKSUM_SIZE)) { // compare calculated checksum to retrieved checksum
+      Serial.print(" (OK)"); // calculated checksum corresponds to transmitted checksum!
+    } else {
+      Serial.print(" (NOT OK)"); // checksums do not match
+    }
 
 
     return tag;
